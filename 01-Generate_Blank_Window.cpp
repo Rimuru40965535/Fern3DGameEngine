9f4aede7e0cc9a167472eae48fd90f988268aced
Rimuru40965535 <3764920947@qq.com>
2026-09-18 05:00:01 +0800

    上传第一个版本。完善文档。

---
+++ 01-Generate_Blank_Window.cpp
@@ -0,0 +1,14 @@
+/***
+* @file 01-Generate_Blank_Window.cpp
+* @brief Implementation for generating a blank window in the Fern 3D game engine.
+* 
+* @author 半人马座beta星
+* @date 2026-09-17
+*/
+
+
+#include "Fern3DGameEngine.h"
+
+int main() {
+	return 0;
+}
